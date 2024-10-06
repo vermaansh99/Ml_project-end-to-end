fd87a3ecf94c7ef72d76f7ffd66e7f40b28c9dce
Ansh verma <anshverma2073@gmail.com>
2024-10-06 14:15:50 +0530

    templates added

---
+++ main.py
@@ -0,0 +1,4 @@
+from mlproject import logger 
+
+
+logger.info("welcome to our custom logging")
--- template.py
+++ template.py
@@ -15,34 +15,37 @@ project_name = "mlproject"
 
 
 #src/__init__.py
+
 list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
-    f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/config/__init__.py",
-    f"src/{project_name}/constants/__init__.py",
-    f"src/{project_name}/entity/config_entity.py",
-    f"src/{project_name}/logger/__init__.py",
-    f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/utils/__init__.py",
-    f"src/{project_name}/utils/commom.py"
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
-    "schema.yaml",
-    "app.py",
-    "main.py",
-    "logs.py",
-    "exception.py",
-    "setup.py",
-    "Dockerfile",
     "params.yaml",
     "schema.yaml",
+    "main.py",
+    "app.py",
+    "Dockerfile",
     "requirements.txt",
+    "setup.py",
     "research/trials.ipynb",
-    "templates/index.html"
-    
+    "templates/index.html",
+    "test.py"
+
 
 ]
+
+    
+
+
 
 
 
